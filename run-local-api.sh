e7c0f01ca020ec3d47c13b1952726d2d2202254c
Steve <leonarduk@users.noreply.github.com>
2025-08-09 07:55:49 +0100

    chore: add backend logging config file

--- run-local-api.sh
+++ run-local-api.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 export ALLOTMINT_ENV=local
-uvicorn backend.local_api.main:app --reload --port 8000
+uvicorn backend.local_api.main:app \
+  --reload \
+  --port 8000 \
+  --log-config backend/logging.ini

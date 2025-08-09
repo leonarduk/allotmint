bfa8fa36d6593e3510beeac83d8661f956079511
Steve <leonarduk@users.noreply.github.com>
2025-08-09 13:55:44 +0100

    Remove account block border

--- frontend/src/components/AccountBlock.tsx
+++ frontend/src/components/AccountBlock.tsx
@@ -24,8 +24,6 @@ export function AccountBlock({ account }: Props) {
       style={{
         marginBottom: "2rem",
         padding: "1rem",
-        border: "1px solid #ddd",
-        borderRadius: "4px",
       }}
     >
       <h2 style={{ marginTop: 0 }}>

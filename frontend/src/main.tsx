e5f045bb59f95550101e7098dfb557f7948ee7ea
Steve <leonarduk@users.noreply.github.com>
2025-08-17 09:58:11 +0100

    Integrate support page into main app

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,7 +4,6 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './index.css'
 import './i18n'
 import App from './App.tsx'
-import Support from './pages/Support'
 import VirtualPortfolio from './pages/VirtualPortfolio'
 import './i18n'
 import { ConfigProvider } from './ConfigContext'
@@ -14,7 +13,6 @@ createRoot(document.getElementById('root')!).render(
     <ConfigProvider>
       <BrowserRouter>
         <Routes>
-          <Route path="/support" element={<Support />} />
           <Route path="/virtual" element={<VirtualPortfolio />} />
           <Route path="/*" element={<App />} />
         </Routes>

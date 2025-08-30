e8997026b277fc1b38352507a16d780b4c658524
Steve <leonarduk@users.noreply.github.com>
2025-08-30 09:22:00 +0100

    cdk: make backend stack optional

--- cdk/app.py
+++ cdk/app.py
@@ -1,10 +1,29 @@
 #!/usr/bin/env python3
+import os
+
 import aws_cdk as cdk
 
 from stacks.backend_lambda_stack import BackendLambdaStack
 from stacks.static_site_stack import StaticSiteStack
 
+
+def _is_truthy(value) -> bool:
+    """Return True for common representations of truthy values."""
+
+    if value is None:
+        return False
+    return str(value).lower() in {"1", "true", "y", "yes"}
+
+
 app = cdk.App()
-BackendLambdaStack(app, "BackendLambdaStack")
+
+deploy_backend_value = (
+    app.node.try_get_context("deploy_backend")
+    or os.getenv("DEPLOY_BACKEND")
+)
+
+if _is_truthy(deploy_backend_value):
+    BackendLambdaStack(app, "BackendLambdaStack")
+
 StaticSiteStack(app, "StaticSiteStack")
 app.synth()

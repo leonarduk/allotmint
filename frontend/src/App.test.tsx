91d33e6193d55d5024ffbaebd8ed0cac856906dc
Steve <leonarduk@users.noreply.github.com>
2025-09-06 22:04:35 +0100

    feat: support base currency selection

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -169,6 +169,8 @@ describe("App", () => {
           tabs: { ...allTabs, movers: false },
           refreshConfig: vi.fn(),
           setRelativeViewEnabled: () => {},
+          baseCurrency: "GBP",
+          setBaseCurrency: () => {},
         }}
       >
         <MemoryRouter initialEntries={["/movers"]}>
@@ -241,6 +243,8 @@ describe("App", () => {
           tabs: allTabs,
           refreshConfig: vi.fn(),
           setRelativeViewEnabled: () => {},
+          baseCurrency: "GBP",
+          setBaseCurrency: () => {},
         }}
       >
         <MemoryRouter initialEntries={["/movers"]}>
